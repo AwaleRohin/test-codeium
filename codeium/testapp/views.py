1499d5eddd3979667443bd4efe7492158402f1a4
rohin331 <rohinawale331@gmail.com>
2024-04-24 11:48:13 +0545

    pre-commit and other code analysis tool add

--- codeium/testapp/views.py
+++ codeium/testapp/views.py
@@ -3,13 +3,13 @@ from .models import Client
 
 
 def add_client(request):
-    if request.method == 'POST':
-        name = request.POST['name']
+    if request.method == "POST":
+        name = request.POST["name"]
         Client.objects.create(name=name)
-        return redirect('client_list')
-    return render(request, 'add_client.html')
+        return redirect("client_list")
+    return render(request, "add_client.html")
 
 
 def client_list(request):
     clients = Client.objects.all()
-    return render(request, 'client_list.html', {'clients': clients})
+    return render(request, "client_list.html", {"clients": clients})
