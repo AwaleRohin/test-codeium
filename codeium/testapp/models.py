fe11f53bda3b64c63ee64ea00e805770dd5c2c68
rohin331 <rohinawale331@gmail.com>
2024-04-24 11:58:16 +0545

    new model add to check code analysis tool

--- codeium/testapp/models.py
+++ codeium/testapp/models.py
@@ -6,3 +6,11 @@ class Client(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
